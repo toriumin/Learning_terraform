5f97ce5f135b5a920f030d9f25f0804991564892
toriumi <saori.toriumi.54@gmail.com>
2018-08-22 20:27:41 +0900

    building infra

---
+++ terraform_example.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+	access_key = "AKIAJDRhogehogehoge"
+	secret_key = "Ce5mzx3piyopiyopiyopiyo"
+	region = "ap-northeast-1"
+	}
+
+resource "aws_instance" "example" {
+	ami = "ami-0834toritoritori"
+	instance_type = "t2.micro"
+	}
